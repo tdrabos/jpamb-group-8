0ff50f4d3791414828dcf266d3e86b101b0aa3b1
Kristina Carter <kscarter2341@gmail.com>
2025-11-24 14:43:18 +0100

    merged main into kristina

--- solutions/interpreter.py
+++ solutions/interpreter.py
@@ -548,7 +548,7 @@ def step(state: State) -> State | str:
                 # assert v1.type is jvm.Boolean(), f"expected bool, but got {v1}"
                 # assert v2.type is jvm.Boolean(), f"expected bool, but got {v2}"
 
-            elif isinstance(v1.type, (jvm.Int, jvm.Boolean, jvm.Float, jvm.Double, jvm.Long))
+            elif isinstance(v1.type, (jvm.Int, jvm.Boolean, jvm.Float, jvm.Double, jvm.Long)):
                 match cond:
                     case "eq": jump = (v1.value == v2.value)
                     case "ne": jump = (v1.value != v2.value)
@@ -577,7 +577,7 @@ def step(state: State) -> State | str:
                     case _:
                         raise NotImplementedError(f"Unknown condition: {cond}")
             
-            elif isinstance(v1.type, (jvm.Int, jvm.Boolean, jvm.Float, jvm.Double, jvm.Long))        
+            elif isinstance(v1.type, (jvm.Int, jvm.Boolean, jvm.Float, jvm.Double, jvm.Long)):        
                 assert v1.type is jvm.Int(), f"expected int, but got {v1}"
                 assert v1.type is jvm.Boolean(), f"expected bool, but got {v1}"
 
